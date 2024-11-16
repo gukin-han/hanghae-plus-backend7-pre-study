fbfba3ae4c55c2fa656daf297d829e617f1c0ee9
Gukin Han <gukin.dev@gmail.com>
2024-11-16 09:59:12 +0900

    chore: 스프링 어플리케이션 초기 설정
    - 도커 환경에서 스프링 어플리케이션 실행
    - H2 데이터베이스에서 빌드, 실행

---
+++ src/test/java/com/example/pre_study/PreStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pre_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
