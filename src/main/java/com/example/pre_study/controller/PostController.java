bcd363268100ebd0f2a744ec4e65947bc698e1bf
Gukin Han <gukin.dev@gmail.com>
2024-11-23 22:17:35 +0900

    feat: 선택한 게시글 조회 API

--- src/main/java/com/example/pre_study/controller/PostController.java
+++ src/main/java/com/example/pre_study/controller/PostController.java
@@ -4,10 +4,7 @@ import com.example.pre_study.controller.request.CreatePostRequest;
 import com.example.pre_study.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +14,16 @@ public class PostController {
 
     @GetMapping("/api/v1/posts")
     public ResponseEntity<?> getPosts() {
-        return ResponseEntity.ok(postService.getPosts());
+        return ResponseEntity.ok(postService.getPost());
     }
 
     @PostMapping("/api/v1/posts")
     public ResponseEntity<?> createPost(@RequestBody CreatePostRequest request) {
         return ResponseEntity.ok(postService.createPost(request));
+    }
+
+    @GetMapping("/api/v1/posts/{postId}")
+    public ResponseEntity<?> getPost(@PathVariable Long postId) {
+        return ResponseEntity.ok(postService.getPostById(postId));
     }
 }
--- src/main/java/com/example/pre_study/service/PostService.java
+++ src/main/java/com/example/pre_study/service/PostService.java
@@ -9,7 +9,6 @@ import com.example.pre_study.service.response.PostResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,12 +16,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostService {
 
-    public static final String USER_NOT_FOUND = "유저를 조회할 수 없습니다.";
+    public static final String ERROR_USER_NOT_FOUND = "유저를 조회할 수 없습니다.";
+    public static final String ERROR_POST_NOT_FOUND = "게시글을 조회할 수 없습니다.";
 
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
-    public List<PostResponse> getPosts() {
+    public List<PostResponse> getPost() {
         final List<Post> posts = postRepository.findPostsOrderByPublishedAtDesc();
 
         return posts.stream()
@@ -33,11 +33,17 @@ public class PostService {
     public PostResponse createPost(CreatePostRequest request) {
 
         final User user = userRepository.findByUsername(request.getUsername())
-                .orElseThrow(() -> new IllegalArgumentException(USER_NOT_FOUND));
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_USER_NOT_FOUND));
 
         final Post post = request.toPostEntity(user, LocalDateTime.now());
         final Post savedPost = postRepository.save(post);
 
         return PostResponse.fromEntity(savedPost);
+    }
+
+    public PostResponse getPostById(Long postId) {
+        final Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_POST_NOT_FOUND));
+        return PostResponse.fromEntity(post);
     }
 }
